3b0c889e8cc4eceeddea99c84f7e1352cf70c855
HREngCode <ccrsailors@gmail.com>
2022-11-11 06:47:21 -0600

    Added Functionality For Fetching Employees By Id

--- backend/employees/views.py
+++ backend/employees/views.py
@@ -11,8 +11,24 @@ from .serializers import EmployeeSerializer
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def employee_list(request):
-    employees = Employee.objects.all()
-    serializer = EmployeeSerializer(employees, many=True)
-    return Response(serializer.data)
+        employees = Employee.objects.all()
+        serializer = EmployeeSerializer(employees, many=True)
+        return Response(serializer.data)
 
     # return Response('ok')
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated]) 
+def user_employees(request):
+    print(
+        'User ', f"{request.user.id} {request.user.email} {request.user.username}")
+    if request.method == 'POST':
+        serializer = EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'GET':
+        employees = Employee.objects.filter(user_id=request.user.id)
+        serializer = EmployeeSerializer(employees, many=True)
+        return Response(serializer.data)

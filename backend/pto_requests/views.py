a86a1180c1533474b1d1feb54f8aeea1908867de
HREngCode <ccrsailors@gmail.com>
2022-11-11 07:13:49 -0600

    Update views.py
    Added Requests For Calling PtoRequest Endpoints

--- backend/pto_requests/views.py
+++ backend/pto_requests/views.py
@@ -5,4 +5,30 @@ from rest_framework.decorators import api_view, permission_classes
 from .models import PtoRequest
 from .serializers import PtoRequestSerializer
 
-# Create your views here.
+# <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_pto_requests(request):
+        ptorequests = PtoRequest.objects.all()
+        serializer = PtoRequestSerializer(ptorequests, many=True)
+        return Response(serializer.data)
+
+    # return Response('ok')
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated]) 
+def user_pto_requests(request):
+    print(
+        'User ', f"{request.user.id} {request.user.email} {request.user.username}")
+    if request.method == 'POST':
+        serializer = PtoRequestSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'GET':
+        ptorequests = PtoRequest.objects.filter(user_id=request.user.id)
+        serializer = PtoRequestSerializer(ptorequests, many=True)
+        return Response(serializer.data)

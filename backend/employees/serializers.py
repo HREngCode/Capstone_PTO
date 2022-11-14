028a84a926e35a72816d9960de58db64f8508275
chad <ccrsailors@gmail.com>
2022-11-14 17:51:09 -0600

    Added Code To PTO Request Serializer To Display Foreign Key

--- backend/employees/serializers.py
+++ backend/employees/serializers.py
@@ -4,10 +4,10 @@ from .models import Employee
 # <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
 
 
-class EmployeeSerializer(serializers.ModelSerializer):
+class EmployeeSerializer(serializers.ModelSerializer): 
     class Meta:
         model = Employee
         fields = ['id', 'employee_number', 'employee_name', 'department', 'supervisor_id', 'hire_date', 'pto_balance', 'active']
-        depth = 1
+        depth = 0
 
-
+    supervisor_id = serializers.IntegerField(write_only=True)

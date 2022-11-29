08b3f556f8fc6b934bc71edd6a488c6256aa45bb
chad <ccrsailors@gmail.com>
2022-11-28 21:16:53 -0600

    Added Code to Autopopulate Fields In Request Based on UserID

--- frontend/src/pages/NewTimeOffRequestPage/NewTimeOffRequestPage.js
+++ frontend/src/pages/NewTimeOffRequestPage/NewTimeOffRequestPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from "../../components/NavBar/NavBar";
 import axios from 'axios';
 import useAuth from '../../hooks/useAuth';
@@ -7,23 +7,8 @@ import {useNavigate} from "react-router-dom"
 const NewTimeOffRequestPage = () => {
     const [user, token] = useAuth ()
     const navigate = useNavigate ()
-
-    async function AddTimeOffRequest(newTimeOffRequest){
-        try 
-        {
-            let response = await axios.post('http://127.0.0.1:8000/api/pto_requests/changes/', newTimeOffRequest, {
-                headers: {
-                    Authorization: 'Bearer ' + token
-                }
-            }) 
-            // navigate("/")
-        console.log(response.data)} 
-        catch (error) 
-        {
-            console.log(error.message)
-        }
-    }
-
+    const [employeeName, setEmployeeName] = useState();
+    const [employeeId, setEmployeeId] = useState();
     // setting up hooks a good place to start
     const [employee, setEmployee] = useState('')
     // const [employeeNumber, setEmployeeNumber] = useState('')
@@ -35,10 +20,43 @@ const NewTimeOffRequestPage = () => {
     // const [supervisor, setSupervisor] = useState('')
     // const [active, setActive] = useState('')
 
+    useEffect(() => {
+        const fetchEmployeeInfo = async () => {
+            try {
+            let response = await axios.get(`http://127.0.0.1:8000/api/employees/user/${user.id}/`, {
+                headers: {
+                Authorization: "Bearer " + token,
+                },
+            });
+            setEmployeeName(response.data.employee_name); 
+            setEmployeeId(response.data.id); 
+            } catch (error) {
+            console.log(error.response.data);
+            }    
+        };
+        fetchEmployeeInfo();
+    }, [token, user]);        
+
+    async function AddTimeOffRequest(newTimeOffRequest){
+        try 
+        {
+            let response = await axios.post('http://127.0.0.1:8000/api/pto_requests/changes/', newTimeOffRequest, {
+                headers: {
+                    Authorization: 'Bearer ' + token
+                }
+            }) 
+            navigate("/")
+        console.log(response.data)} 
+        catch (error) 
+        {
+            console.log(error.message)
+        }
+    }    
+
     function handleSubmit(event){
         event.preventDefault();
         let newTimeOffRequest = {
-            employee__id: employee,
+            employeeId: employeeId,
             // employee_number: employeeNumber,
             // department: department,
             // hire_date: hireDate,
@@ -56,9 +74,10 @@ const NewTimeOffRequestPage = () => {
                 <div>
                     <form onSubmit={handleSubmit}>
                     {/* <form> */}
+                    
                         <div className='newEntry'>
-                        <label>Employee: </label>
-                        <input value={employee} onChange={(event) => setEmployee(event.target.value)}/>
+                        <label>Employee Id: </label>
+                        <input type="number" value={employeeId} onChange={(event) => setEmployee(event.target.value)}/>
                         </div>
                         <div className='newEntry'>
                         <label>Date Requested Off: </label>

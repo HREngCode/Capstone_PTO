48a673c61b1180d91dd892e67499b34addea8fa2
chad <ccrsailors@gmail.com>
2023-03-14 13:34:05 -0500

    Added View For Put And Delete Calls

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -4,7 +4,9 @@ from . import views
 # <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
 
 urlpatterns = [
-    path('number/<id>/', views.comments_list),
+    path('employee/<id>/', views.comments_list_by_employee),
+    path('request/<id>/', views.comments_list_by_request),
+    path('update/<pk>/', views.comments_detail),
     path('all/', views.get_all_comments),
     path('changes/', views.user_comments),
 ]
--- backend/comments/views.py
+++ backend/comments/views.py
@@ -9,10 +9,19 @@ from .serializers import CommentSerializer
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
-def comments_list(request, id):
+def comments_list_by_employee(request, id):
         comments = request.query_params.get(id)
         queryset = Comment.objects.all()
-        queryset = queryset.filter(video_id=id)
+        queryset = queryset.filter(employee_id=id)
+        serializer = CommentSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def comments_list_by_request(request, id):
+        comments = request.query_params.get(id)
+        queryset = Comment.objects.all()
+        queryset = queryset.filter(pto_request_id=id)
         serializer = CommentSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -31,10 +40,30 @@ def user_comments(request):
     if request.method == 'POST':
         serializer = CommentSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(user=request.user)
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'GET':
-        comments = Comment.objects.filter(user_id=request.user.id)
+        comments = Comment.objects.filter(employee_id=request.employee.id)
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data)
+    
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([AllowAny]) 
+def comments_detail(request, pk):
+    comment = Comment.objects.get(pk=pk)
+    if request.method == 'GET':
+        try:
+            serializer = CommentSerializer(comment)
+            return Response(serializer.data)
+        except Comment.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    elif request.method == 'PUT':
+        serializer = CommentSerializer(comment, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
